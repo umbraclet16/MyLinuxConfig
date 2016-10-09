363181f8e03790b42bd382f325b8a4d40ad9fed6
umbraclet16 <448101470@qq.com>
2016-10-09 11:41:02 +0800

    Add instructions to modify '.bashrc' for '.compile_opencv.sh'

--- .compile_opencv.sh
+++ .compile_opencv.sh
@@ -10,3 +10,19 @@ else
     echo "Please compile only .c or .cpp files"
 fi
 echo "Output file => ${1%.*}"
+
+#############################################
+# Add an alias in .bashrc or .bash_aliases
+# (https://help.ubuntu.com/community/OpenCV)
+#############################################
+#
+# alias opencv="~/.compile_opencv.sh"
+# opencv opencvtest.c
+# ./opencvtest
+#
+#############################################
+# Note that the .bashrc is a hidden file in the $HOME directory 
+# (cd ~ to access the home directory).
+# The alias will work after you log out and back.
+# You can type the alias opencv... command at the prompt to 
+# set the alias for the current session.
